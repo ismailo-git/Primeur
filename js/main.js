8996437b6817d152d47e63e6a3ff8f51c3dd179f
ismail <hassanliamsi@gmail.com>
2021-03-30 21:47:25 +0200

    Finish for today

--- js/main.js
+++ js/main.js
@@ -4,5 +4,14 @@ $(document).ready(function () {
 
         $(this).toggleClass('fa-times');
         $('.navbar').toggleClass('nav-toggle');
-    })
+    });
+
+    $('.btn-commander').click(function () {
+
+        $("#notification").fadeIn("slow").html('Désolé ce site est un site vitrine. Cette fonction ne fonctionne pas. Merci pour votre comprehension ');
+        $("#notification").fadeOut(8000);
+    });
+
+
+
 });
